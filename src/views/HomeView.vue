aca6cc1881d93d3bebe94dd11809b25ea56a77be
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-19 19:37:57 +0100

    Created MyList

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="home">
       <h1>home</h1>
+      <input type="text">
+      <MyList />
     </div>
 </template>
 
 <script>
+import MyList from '@/components/MyList.vue';
 
 export default {
   name: 'HomeView',
   components: {
-
+    MyList
   }
 }
 </script>
