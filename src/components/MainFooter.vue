9cf6ddc9c3e0f775716d3abd9a3cc997b5feffb1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-21 22:41:51 +0100

    Updated MainFooter

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,7 +1,17 @@
 <template>
-  <p>I am a footer</p>
+  <footer>Coded by <a href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a>, open-sourced on 
+  <a href="https://github.com/Lu-Smith/todo_list.vue" target="_blank">GitHub</a>.</footer>
 </template>
 
 <style>
+footer {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    text-align: center;
+    background-color: #f2f2f2;
+  padding: 20px;
 
+}
 </style>
