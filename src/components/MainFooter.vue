2912ff93365bd85d4e32e53d19dbd24617b20fc5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-21 22:44:57 +0100

    Updated styles in MainFooter

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -11,7 +11,38 @@ footer {
     left: 0;
     text-align: center;
     background-color: #f2f2f2;
-  padding: 20px;
+    padding: 20px;
 
 }
+
+footer a {
+  color: #333;
+  text-decoration: none;
+  position: relative;
+  transition: color 0.3s;
+}
+
+footer a::before {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  bottom: -5px;
+  left: 0;
+  background-color: #333;
+  visibility: hidden;
+  transform: scaleX(0);
+  transition: all 0.3s ease-in-out;
+}
+
+footer a:hover {
+  color: #d17c37;
+}
+
+footer a:hover::before {
+  visibility: visible;
+  transform: scaleX(1);
+}
+
+
 </style>
