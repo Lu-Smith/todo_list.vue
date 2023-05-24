957e5b0086f9d6b90c467726ac1fd8361023b76c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-24 16:27:06 +0100

    Updated MyList.vue

--- src/components/MyList.vue
+++ src/components/MyList.vue
@@ -1,5 +1,6 @@
 <template>
   <h2 v-if="tasks.length > 0">{{ listTitle }}</h2>
+  <h2 v-else>Woohoo, nothing to do!</h2>
   <div v-for="task in tasks" :key="task">
       <div @click="markDoneTask(task)" :class="{'task': true, 'done-task': task.done}">
         <span v-if="task.done" class="material-symbols-outlined" >
