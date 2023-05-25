f9049f8c68cff91bc341d57f0d309256e666c8e6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-25 18:48:45 +0100

    Updated ContactView

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,6 +1,37 @@
 <template>
     <div class="contact">
-      <h1>This is an contact page</h1>
+      <h1>Contact</h1>
+      <ul>
+        <li v-for="icon in icons" :key="icon.name">
+          <span class="material-icons">{{ icon.name }}</span>
+          <div>{{ icon.text }}</div>
+        </li>
+      </ul>
     </div>
   </template>
+
+  <script>
+    import { ref } from 'vue'
+
+    export default {
+      setup() {
+        const icons = ref([
+          {
+            name: 'alternate_email', text: 'by email'
+          },
+          {
+            name: 'local_phone', text: 'by phone'
+          },
+          {
+            name: 'local_post_office', text: 'by post'
+          },
+          {
+            name: 'local_fire_department', text: 'by smoke signal'
+          }
+        ])
+
+        return { icons }
+      }
+    }
+</script>
   
